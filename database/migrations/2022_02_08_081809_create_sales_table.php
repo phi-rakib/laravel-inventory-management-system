a81f65498d76acf553e053e3d8b71faad12618e9
rakib <phi.rakib@gmail.com>
2022-02-08 23:07:44 +0600

    created sales table

---
+++ database/migrations/2022_02_08_081809_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->uuid('order_id');
+            $table->unsignedBigInteger('purchase_id');
+            $table->decimal('price');
+            $table->decimal('discount')->default(0);
+            $table->decimal('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
