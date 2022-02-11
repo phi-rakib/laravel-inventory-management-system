879861431f17a8ff04d55c06d0be641f0bd187df
rakib <phi.rakib@gmail.com>
2022-02-11 22:10:48 +0600

    getting all the products from product and productDetails table by relation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $products = Product::with("productDetails")->get();
         return $products;
     }
 }
