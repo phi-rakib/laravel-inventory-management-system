85785fea6c0272b8fcb57afd09d510cada959e30
rakib <phi.rakib@gmail.com>
2022-02-12 17:38:34 +0600

    added delete by id method route

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::post('/product', [ProductController::class, 'store']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
