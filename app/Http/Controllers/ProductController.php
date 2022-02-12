c7ac8293674b654afdff461c79786e4f96abe93a
rakib <phi.rakib@gmail.com>
2022-02-12 15:39:39 +0600

    added product search

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $searchTerm = $request->query('q');
+
         $products = Product::with("productDetails:id,product_id,description")
+            ->where('name', 'like', "%{$searchTerm}%")
             ->simplePaginate(10);
+            
         return $products;
     }
 }
