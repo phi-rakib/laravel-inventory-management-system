48e82d4c70ff509bcae0f18dbf9c5eb4d7ccd8d3
rakib <phi.rakib@gmail.com>
2022-02-14 15:34:41 +0600

    added user login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -13,5 +14,25 @@ class AuthController extends Controller
         $user = User::create($input);
         $token = $user->createToken('MyAuthApp')->plainTextToken;
         return ["token" => $token];
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        if (Auth::attempt($input)) {
+            $user = User::where('email', $input['email'])->firstOrFail();
+
+            $user_agent = $request->header('User-Agent');
+
+            $token = $user->createToken($user_agent)->plainTextToken;
+
+            return ["token" => $token];
+        }
+
+        return response()->json(
+            ['message' => 'Invalid login details'],
+            401
+        );
     }
 }
