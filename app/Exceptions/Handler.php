8ee34e78bb3a3b607a1eee91c90728496f47d02f
rakib <phi.rakib@gmail.com>
2022-02-14 14:18:17 +0600

    added function render to get json response when any error occur

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,13 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +42,46 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(
+                [
+                    'error' => 'Resource not found',
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json(
+                [
+                    'error' => 'The specified method for the request is invalid',
+                ],
+                Response::HTTP_METHOD_NOT_ALLOWED
+            );
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json(
+                [
+                    'error' => 'The specified URL cannot be found',
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->json(
+                [
+                    'error' => $exception->getMessage(),
+                ],
+                $exception->getStatusCode()
+            );
+        }
+
+        return parent::render($request, $exception);
     }
 }
