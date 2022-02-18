615bae55d02d799fdf202ee22b5520d084219f40
rakib <phi.rakib@gmail.com>
2022-02-18 14:52:35 +0600

    added auth middleware sanctum

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');    
+    }
+
     public function index()
     {
         $parents = Category::whereNull('parent_id')->get();
