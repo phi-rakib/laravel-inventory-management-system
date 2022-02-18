f4adaf81ef3141c912c9d62512cb96e9846cfa51
rakib <phi.rakib@gmail.com>
2022-02-18 19:55:38 +0600

    rearranged routes

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::post('/product', [ProductController::class, 'store']);
-Route::delete('/product/{id}', [ProductController::class, 'destroy']);
-Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::put('/product/{product}', [ProductController::class, 'update']);
+Route::delete('/product/{product}', [ProductController::class, 'destroy']);
 
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::post('/category', [CategoryController::class, 'store']);
 Route::put('/category/{category}', [CategoryController::class, 'update']);
 Route::delete('/category/{category}', [CategoryController::class, 'destroy']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
 Route::post('/user/registration', [AuthController::class, 'store']);
 Route::post('/user/login', [AuthController::class, 'login']);
