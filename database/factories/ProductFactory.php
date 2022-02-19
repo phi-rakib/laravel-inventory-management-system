5262e6b2e1f31a952edc8ef26e30b5a46f93d59c
rakib <phi.rakib@gmail.com>
2022-02-19 14:18:21 +0600

    created product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'summary' => $this->faker->sentence(),
+            'brand_id' => null,
+            'category_id' => null,
+            'description' => $this->faker->text(),
+        ];
+    }
+}
