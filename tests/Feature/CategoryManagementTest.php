189dd70b2f029c56b7bcfadedaf0e3098c6f786c
rakib <phi.rakib@gmail.com>
2022-02-19 17:39:26 +0600

    added user and category factory

--- tests/Feature/CategoryManagementTest.php
+++ tests/Feature/CategoryManagementTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use App\Models\User;
 use App\Models\Category;
-use Laravel\Sanctum\Sanctum;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
 
 class CategoryManagementTest extends TestCase
 {
@@ -16,36 +16,27 @@ class CategoryManagementTest extends TestCase
     {
         parent::setUp();
 
-        Sanctum::actingAs(User::create([
-            'role_id' => 1,
-            'name' => 'rakib',
-            'email' => 'phi.rakib@gmail.com',
-            'password' => 'abc',
-        ]), ['*']);
+        Sanctum::actingAs(User::factory()->create(), ['*']);
     }
 
     /** @test */
     public function should_fetch_all_categories()
     {
-        $this->withoutExceptionHandling();
-
         Category::factory()->count(5)->child()->create();
-        
+
         $response = $this->get('/api/category');
 
         $response->assertOk();
 
         $response->assertJsonCount(5);
     }
-    
 
     /** @test */
     public function a_category_can_be_added()
     {
-        $response = $this->post('/api/category', [
-            'name' => 'noodles',
-            'parent_id' => null,
-        ]);
+        $category = Category::factory()->make()->toArray();
+
+        $response = $this->post('/api/category', $category);
 
         $response->assertCreated();
 
@@ -55,10 +46,9 @@ class CategoryManagementTest extends TestCase
     /** @test */
     public function a_name_is_required()
     {
-        $response = $this->post('/api/category', [
-            'name' => '',
-            'parent_id' => null,
-        ]);
+        $category = Category::factory()->state(['name' => ''])->make()->toArray();
+
+        $response = $this->post('/api/category', $category);
 
         $response->assertSessionHasErrors('name');
     }
@@ -66,10 +56,9 @@ class CategoryManagementTest extends TestCase
     /** @test */
     public function parent_id_can_be_nullable_or_numeric()
     {
-        $response = $this->post('/api/category', [
-            'name' => '',
-            'parent_id' => 'abc',
-        ]);
+        $category = Category::factory()->state(['parent_id' => 'abc'])->make()->toArray();
+
+        $response = $this->post('/api/category', $category);
 
         $response->assertSessionHasErrors('parent_id');
     }
@@ -77,10 +66,7 @@ class CategoryManagementTest extends TestCase
     /** @test */
     public function a_category_can_be_updated()
     {
-        $response = $this->post('/api/category', [
-            'name' => 'noodles',
-            'parent_id' => null,
-        ]);
+        $response = $this->post('/api/category', Category::factory()->make()->toArray());
 
         $response->assertCreated();
 
@@ -88,23 +74,20 @@ class CategoryManagementTest extends TestCase
 
         $category = Category::first();
 
-        $this->put('/api/category/' . $category->id, [
-            'name' => 'cook noodles',
-            'parent_id' => 1,
-        ]);
+        $tmpCategory = Category::factory()->state(['parent_id' => 1])->make();
+
+        $this->put('/api/category/' . $category->id, $tmpCategory->toArray());
 
         $updated_category = Category::first();
-        $this->assertEquals('cook noodles', $updated_category->name);
-        $this->assertEquals(1, $updated_category->parent_id);
+
+        $this->assertEquals($tmpCategory->name, $updated_category->name);
+        $this->assertEquals($tmpCategory->parent_id, $updated_category->parent_id);
     }
 
     /** @test */
     public function a_category_can_be_deleted()
     {
-        $response = $this->post('/api/category', [
-            'name' => 'noodles',
-            'parent_id' => null,
-        ]);
+        $response = $this->post('/api/category', Category::factory()->make()->toArray());
 
         $response->assertCreated();
 
