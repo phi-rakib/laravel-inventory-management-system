a2881199104ac60b8ac180c080c4afd98e199512
rakib <phi.rakib@gmail.com>
2022-02-20 22:10:00 +0600

    renamed get all product route products to product

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::post('/product', [ProductController::class, 'store']);
 Route::put('/product/{product}', [ProductController::class, 'update']);
