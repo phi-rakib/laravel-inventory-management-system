4d3ace8bea973ce98c11009442a142794f1e88ed
rakib <phi.rakib@gmail.com>
2022-02-20 14:58:22 +0600

    added uuid as a primary key as well as used observer hook creating for generating uuid

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Purchase;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
 
     protected $fillable = [
         'user_id',
@@ -33,5 +40,12 @@ class Order extends Model
     public function purchases()
     {
         return $this->hasMany(Purchase::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function($product){
+            $product->id = Str::uuid();
+        });
     }
 }
