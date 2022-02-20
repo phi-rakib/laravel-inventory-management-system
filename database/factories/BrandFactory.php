c6982fad2e0f108e1d64832c8c167a5cd3627e05
rakib <phi.rakib@gmail.com>
2022-02-20 08:31:36 +0600

    added brand name unique

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -14,7 +14,7 @@ class BrandFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word(),
+            'name' => $this->faker->unique()->domainWord(),
             'description' => $this->faker->sentence()
         ];
     }
