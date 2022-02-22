06db0247cce5905f7d5fdcb3781e23507c1e4136
rakib <phi.rakib@gmail.com>
2022-02-22 13:47:24 +0600

    replaced hardcoded value with a constant

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
@@ -16,7 +17,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'role_id' => 1,
+            'role_id' => Config::get('constants.roles.admin'),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
