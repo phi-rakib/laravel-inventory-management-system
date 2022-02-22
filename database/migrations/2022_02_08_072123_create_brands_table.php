65942964491afd6a6ccc187799b1107f84a759b5
rakib <phi.rakib@gmail.com>
2022-02-22 15:11:51 +0600

    added column updated_by in the brand table

--- database/migrations/2022_02_08_072123_create_brands_table.php
+++ database/migrations/2022_02_08_072123_create_brands_table.php
@@ -18,6 +18,7 @@ class CreateBrandsTable extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamps();
         });
     }
