98666c0575a2a952e726a371135386ad7eb4f78d
rakib <phi.rakib@gmail.com>
2022-02-23 17:24:47 +0600

    added created_by column and filled in by random user id in brand seeder.

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Brand;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Config;
 
@@ -15,8 +17,14 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        Brand::factory()
-            ->count(Config::get('constants.test.brand.max_item'))
-            ->create();
+        Brand::withoutEvents(function () {
+            Brand::factory()
+                ->count(Config::get('constants.test.brand.max_item'))
+                ->state(new Sequence(
+                    fn($sequence) => [
+                        'created_by' => User::inRandomOrder()->first()->id,
+                    ]
+                ))->create();
+        });
     }
 }
