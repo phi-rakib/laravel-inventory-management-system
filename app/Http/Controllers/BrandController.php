138fb5351caa6b83405317becbe0eac9d742af86
rakib <phi.rakib@gmail.com>
2022-02-24 22:03:50 +0600

    moved validation into BrandRequest class

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandRequest;
 use App\Models\Brand;
 use Illuminate\Support\Facades\Config;
 
@@ -22,26 +23,18 @@ class BrandController extends Controller
         return $brand;
     }
 
-    public function store()
+    public function store(BrandRequest $request)
     {
-        Brand::create($this->validateRequest());
+        Brand::create($request->validated());
     }
 
-    public function update(Brand $brand)
+    public function update(Brand $brand, BrandRequest $request)
     {
-        $brand->update($this->validateRequest());
+        $brand->update($request->validated());
     }
 
     public function destroy(Brand $brand)
     {
         $brand->delete();
-    }
-
-    protected function validateRequest()
-    {
-        return request()->validate([
-            'name' => 'required',
-            'description' => '',
-        ]);
     }
 }
