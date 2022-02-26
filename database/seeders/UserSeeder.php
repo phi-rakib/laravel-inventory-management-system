bc041372776cf9486be974f248d402b683ef4316
rakib <phi.rakib@gmail.com>
2022-02-26 14:25:19 +0600

    added an admin user into the userSeeder with hardcoded email and password.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,8 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->state([
+            'email' => 'admin@test.com',
+            'password' => '123456',
+        ])->create();
+
         User::factory()
-            ->count(Config::get('constants.test.user.max_item'))
+            ->count(Config::get('constants.test.user.max_item') - 1)
             ->state(new Sequence(
                 fn($sequence) => ['role_id' => rand(1, 4)]
             ))->create();
